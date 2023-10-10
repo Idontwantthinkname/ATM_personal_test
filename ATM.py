f6c749a0d7ebbe096e0902cee996e0a424467f26
Yujiu <144003330+Yujiu-Sama@users.noreply.github.com>
2023-10-10 15:35:19 +0800

    Add files via upload

--- ATM.py
+++ ATM.py
@@ -7,6 +7,7 @@ def Balance():
     Balance: 余额
     :return: 返回None
     """
+    print("-------------------------------余额------------------------------")
     print(f"{name}，您好，您的余额剩余：{money}")
     main_menu()
 
@@ -16,6 +17,7 @@ def Deposit():
     Deposit: 存款
     :return: 返回None
     """
+    print("-------------------------------存款------------------------------")
     money_in = int(input("请输入存款金额"))
     print(f"{name}，您好，您存款{money_in}元成功")
     global money
@@ -28,6 +30,7 @@ def Withdraw():
     Withdraw: 取款
     :return: 返回None
     """
+    print("-------------------------------取款------------------------------")
     money_out = int(input("请输入取款金额"))
     global money
     if money_out <= money:
@@ -38,10 +41,11 @@ def Withdraw():
 
 def main_menu():
     """
-    main_menu: 主菜单
-    :return: 结束程序
-    """
-    print(f"{name}，您好，欢迎来到银行ATM，请选择你的操作")
+        main_menu: 主菜单
+        :return: 结束程序
+        """
+    print("------------------------------主菜单------------------------------")
+    print(f"{name}，您好，欢迎来到黑马银行ATM，请选择你的操作")
     print("查询余额\t[输入1]\n存款\t\t[输入2]\n取款\t\t[输入3]\n退出\t\t[输入4]")
     num = int(input("请输入您的选择:\n"))
 
