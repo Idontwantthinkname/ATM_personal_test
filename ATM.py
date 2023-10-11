3fef543e9809a362037bbf4d7f1c100f6bd52529
Yujiu <144003330+Yujiu-Sama@users.noreply.github.com>
2023-10-11 18:43:23 +0800

    Add files via upload

--- ATM.py
+++ ATM.py
@@ -2,14 +2,14 @@ money = 5000000
 name = input("请输入你的姓名: ")
 
 
-def Balance():
+def Balance(show_header):
     """
     Balance: 余额
     :return: 返回None
     """
-    print("-------------------------------余额------------------------------")
-    print(f"{name}，您好，您的余额剩余：{money}")
-    main_menu()
+    if show_header:
+        print("-------------------------------余额------------------------------")
+    print(f"{name}，您好，您的余额剩余：{money}。")
 
 
 def Deposit():
@@ -19,10 +19,10 @@ def Deposit():
     """
     print("-------------------------------存款------------------------------")
     money_in = int(input("请输入存款金额"))
-    print(f"{name}，您好，您存款{money_in}元成功")
+    print(f"{name}，您好，您存款{money_in}元成功。")
     global money
     money += money_in
-    Balance()
+    Balance(False)
 
 
 def Withdraw():
@@ -31,12 +31,16 @@ def Withdraw():
     :return: 返回None
     """
     print("-------------------------------取款------------------------------")
-    money_out = int(input("请输入取款金额"))
     global money
+    money_out = int(input("请输入取款金额"))
+
     if money_out <= money:
-        print(f"{name}，您好，您取款{money_out}元成功")
+        print(f"{name}，您好，您取款{money_out}元成功。")
         money -= money_out
-        Balance()
+        Balance(False)
+    else:
+        print("您的余额不足。")
+        Balance(False)
 
 
 def main_menu():
@@ -45,21 +49,22 @@ def main_menu():
         :return: 结束程序
         """
     print("------------------------------主菜单------------------------------")
-    print(f"{name}，您好，欢迎来到黑马银行ATM，请选择你的操作")
+    print(f"{name}，您好，欢迎来到ATM，请选择你的操作。")
     print("查询余额\t[输入1]\n存款\t\t[输入2]\n取款\t\t[输入3]\n退出\t\t[输入4]")
-    num = int(input("请输入您的选择:\n"))
+    return input("请输入您的选择:\n")
 
-    if num == 1:
-        Balance()
 
-    if num == 2:
+while True:
+    keyboard_input = main_menu()
+    if keyboard_input == "1":
+        Balance(True)
+        continue
+    elif keyboard_input == "2":
         Deposit()
-
-    if num == 3:
+        continue
+    elif keyboard_input == "3":
         Withdraw()
-
-    if num == 4:
-        return None
-
-
-main_menu()
+        continue
+    else:
+        print("程序退出")
+    break  # 通过break退出程序
